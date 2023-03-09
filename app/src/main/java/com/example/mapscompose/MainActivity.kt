78d74529fe9fc5e3c3a29729b0b2840a5cbf0ac7
Juan Estrella <jlkmoy@gmail.com>
2023-03-09 12:43:55 -0500

    Update: init google map

--- app/src/main/java/com/example/mapscompose/MainActivity.kt
+++ app/src/main/java/com/example/mapscompose/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapscompose.ui.theme.MapsComposeTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Google_Maps()
                 }
             }
         }
@@ -30,14 +35,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Google_Maps() {
+    val location = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(location, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ){
+        Marker(
+            state = MarkerState(position = location),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun Google_Maps_preview() {
     MapsComposeTheme {
-        Greeting("Android")
+        Google_Maps()
     }
 }
