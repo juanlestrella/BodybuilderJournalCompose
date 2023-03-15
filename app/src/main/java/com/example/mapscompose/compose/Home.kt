3f4c6e99c21547c17741cd67a37e72165490eee8
Juan Estrella <jlkmoy@gmail.com>
2023-03-15 14:26:50 -0400

    Update: lazies and scrollable

--- app/src/main/java/com/example/mapscompose/compose/Home.kt
+++ app/src/main/java/com/example/mapscompose/compose/Home.kt
@@ -1,10 +1,13 @@
 package com.example.mapscompose.compose
 
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -15,53 +18,83 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mapscompose.R
 import com.example.mapscompose.ui.theme.MapsComposeTheme
 
 
-//todo add scrollable and lazy layouts
+/**
+ * TODO:
+ * 1) Apply Card Effects
+ * 2) Make each Card clickable
+ * 3) Implement Scaffold
+ * 4) Implement network
+ * 5) Implement entities and database
+ * 6) Implement ViewModels
+ * 7) Implement repositories
+ * 8) Implement navigation
+ */
 
 @Composable
-fun Home_Screen() {
-    Column(){ // Needs to be Lazy
-        Spacer(modifier = Modifier.height(16.dp)) // this helps avoid recomposition of the child composable function
-        LocationBar()
-        Spacer(modifier = Modifier.height(16.dp))
-        LocationBar()
-    }
-}
-
-@Composable
-fun LocationBar(){
-    Column(){
-        Text(text = "Location Name")
-        Row() {// Needs to be Lazy
-            ContentCard()
-            ContentCard()
-            ContentCard()
+fun Home_Screen(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.padding(vertical = 4.dp),
+        userScrollEnabled = true
+    ){ // Needs to be Lazy
+        items(10){
+            Spacer(modifier = modifier.height(16.dp)) // this helps avoid recomposition of the child composable function
+            LocationBar()
         }
     }
 }
 
 @Composable
-fun ContentCard(){
-    Card(){
-        Column(){
-            val imageModifier = Modifier
-                .size(150.dp)
-                .border(BorderStroke(1.dp, Color.Black))
-                .background(Color.Gray)
-                .fillMaxWidth()
-            Image(
-                painter = painterResource(id = R.drawable.baseline_smart_toy_24),
-                contentDescription = "temp description",
-                contentScale = ContentScale.Fit,
-                modifier = imageModifier)
-            Text(text = "Title")
-            Text(text = "Date")
+fun LocationBar(
+    modifier: Modifier = Modifier,
+    location: String = "Location Name"
+){
+    Column{
+        Text(text = location, modifier = modifier.padding(4.dp), fontSize = 32.sp)
+        Card(
+            //todo: apply effects
+        ){
+            LazyRow(
+                userScrollEnabled = true
+            ) {
+                items(10){
+                    ContentCard()
+                }
+            }
         }
     }
+}
 
+@Composable
+fun ContentCard(
+    modifier: Modifier = Modifier,
+    @DrawableRes image: Int = R.drawable.baseline_smart_toy_24,
+    description: String = "temp description",
+    title: String = "Caption",
+    date: String = "Date"
+){
+    Column(
+        modifier = modifier.padding(horizontal = 4.dp)
+    ){
+        val imageModifier = modifier
+            .size(150.dp)
+            .border(BorderStroke(1.dp, Color.Black))
+            .background(Color.Gray)
+            .fillMaxWidth()
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = description,
+            contentScale = ContentScale.Fit,
+            modifier = imageModifier)
+        Text(text = date)
+        Text(text = title)
+    }
 }
 
 @Preview
