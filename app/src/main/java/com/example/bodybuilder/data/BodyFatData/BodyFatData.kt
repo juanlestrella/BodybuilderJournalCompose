1150778e0adbab27514ee3c38136ce8668e27f9b
juanlestrella <juan.lestrella03@gmail.com>
2023-05-14 18:31:14 -0400

    Update: MacrosCalculatorVM fixed, next update UIs

--- app/src/main/java/com/example/bodybuilder/data/BodyFatData/BodyFatData.kt
+++ app/src/main/java/com/example/bodybuilder/data/BodyFatData/BodyFatData.kt
@@ -5,14 +5,9 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class BodyFatData(
-    @Json(name = "Body Fat (U.S. Navy Method)")
-    val bodyFat: Float?,
-    @Json(name = "Body Fat Category")
-    val category: String?,
-    @Json(name = "Body Fat Mass")
-    val bodyFatMass: Float?,
-    @Json(name = "Lean Body Mass")
-    val leanBodyMass: Float?,
-    @Json(name = "Body Fat (BMI method)")
-    val bmi: Float?
+    @Json(name = "Body Fat (U.S. Navy Method)") val bodyFat: Float?,
+    @Json(name = "Body Fat Category") val category: String?,
+    @Json(name = "Body Fat Mass") val bodyFatMass: Float?,
+    @Json(name = "Lean Body Mass") val leanBodyMass: Float?,
+    @Json(name = "Body Fat (BMI method)") val bmi: Float?
 )
--- app/src/main/java/com/example/bodybuilder/data/MacrosData/MacrosData.kt
+++ app/src/main/java/com/example/bodybuilder/data/MacrosData/MacrosData.kt
@@ -1,12 +1,13 @@
 package com.example.bodybuilder.data.MacrosData
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class MacrosData(
     var calorie: Float?,
     var balanced: MacrosDetail,
-    var lowFat: MacrosDetail,
-    var lowCarbs: MacrosDetail,
-    var highProtein: MacrosDetail
+    @Json(name = "lowfat") var lowFat: MacrosDetail,
+    @Json(name = "lowcarbs") var lowCarbs: MacrosDetail,
+    @Json(name = "highprotein") var highProtein: MacrosDetail
 )
