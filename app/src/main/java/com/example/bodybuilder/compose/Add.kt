983337510dc989ca8221401abebad8d67f78e906
juanlestrella <juan.lestrella03@gmail.com>
2023-05-17 13:04:37 -0400

    Update: Add Screen Images included

--- app/src/main/java/com/example/bodybuilder/compose/Add.kt
+++ app/src/main/java/com/example/bodybuilder/compose/Add.kt
@@ -1,10 +1,16 @@
 package com.example.bodybuilder.compose
 
 import android.app.DatePickerDialog
+import android.media.Image
+import android.net.Uri
 import android.widget.DatePicker
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -14,7 +20,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.util.Calendar
 
@@ -48,16 +59,34 @@ fun AddBodyContent(
             date = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
         }, year, month, day
     )
-    Column(
-        modifier = modifier.fillMaxSize(),
-        //verticalArrangement = Arrangement.Center,
-        //horizontalAlignment = Arrangement.CenterHorizontally
-    ) {
-        Text(text = if(date.isNotEmpty()) "$date" else "Please add a date")
 
-        Button( onClick = { datePicker.show() }){
-            Text(text = "Select Date")
+    var selectedImages by rememberSaveable{ mutableStateOf(listOf<Uri>())}
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetMultipleContents(),
+        onResult = { selectedImages = it }
+    )
+
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)){
+            Button( onClick = { datePicker.show() }){
+                Text(text = if(date.isNotEmpty()) "$date" else "Select Date")
+            }
+            Button( onClick = {launcher.launch("image/*")}){
+                Text(text = "Pick Images")
+            }
         }
+        LazyColumn(content = {
+            items(selectedImages.size) { index ->
+                AsyncImage(
+                    model = selectedImages[index],
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+            }
+        })
     }
 
 }
